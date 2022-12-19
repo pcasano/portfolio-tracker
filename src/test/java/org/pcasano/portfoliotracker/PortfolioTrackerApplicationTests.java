f1e8f0437f05911214d7b9c817466c8e9f58a14e
pcasano <pablo.casanovas@pm.me>
2022-12-19 18:18:37 +0100

    added

---
+++ src/test/java/org/pcasano/portfoliotracker/PortfolioTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pcasano.portfoliotracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
